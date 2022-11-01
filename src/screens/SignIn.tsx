ff98039a1bf5a09a486a25bf3d63dd8907594297
Vinicius <vinicardoso216@gmail.com>
2022-11-01 17:33:42 -0300

    feat: Loading component created

---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,14 @@
+import { Text, Center } from "native-base";
+
+
+export function SignIn() {
+    return (
+        <Center flex={1} bgColor="gray.900" fontFamily="heading">
+
+            <Text color="white" fontSize={40}>
+            SignIn
+            </Text>
+
+        </Center>
+    )
+}
