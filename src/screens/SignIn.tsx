a03c4e27c829a106bde9c9a204ed817eb3955245
Vinicius <vinicardoso216@gmail.com>
2022-11-03 10:16:46 -0300

    feat: Authentication interface created

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,12 +1,29 @@
-import { Text, Center } from "native-base";
+import { Text, Center, Icon } from "native-base";
+import { Fontisto } from '@expo/vector-icons';
+
+import Logo from '../assets/logo.svg';
+
+import { Button } from '../components/Button';
 
 
 export function SignIn() {
     return (
-        <Center flex={1} bgColor="gray.900" fontFamily="heading">
+        <Center flex={1} bgColor="gray.900" p={7}>
+            <Logo width={212} height={40} />
 
-            <Text color="white" fontSize={40}>
-            SignIn
+            <Button 
+                type="SECONDARY"
+                title="LOGIN WITH GOOGLE"
+                leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+                mt={12}
+            />
+
+            <Text
+                color="white"
+                textAlign="center"
+                mt={4}
+            >
+                We do not use any information other than {'\n'} your email to create your account.
             </Text>
 
         </Center>
